9d30b3299a2750c2e466665eb37cb4479b776861
Shadowgamesps5 <jonatan_luiz_jb@hotmail.com>
2025-06-29 00:43:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ async function carregarLikes() {
         const data = await res.json();
 
         const container = document.getElementById("like-container");
-        container.innerHTML = ""; // limpa antes
+        container.innerHTML = ""; // Limpa o anterior
 
-        data.forEach(item => {
+        if (data.length > 0) {
+            const item = data[0]; // Mostra só o primeiro
             const div = document.createElement("div");
             div.className = "like-box";
             div.innerHTML = `${item.nome} +${item.likes} <span class="heart">❤️</span>`;
             container.appendChild(div);
-        });
+        }
     } catch (error) {
         console.error("Erro ao carregar JSON:", error);
     }
